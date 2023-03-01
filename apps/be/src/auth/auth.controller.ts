d087438f4b977fad4b34dec7b655e86689c1f705
Nguyen Tai <nguyentai21.fit@gmail.com>
2023-03-01 17:38:59 +0700

    api: add swagger

--- apps/be/src/auth/auth.controller.ts
+++ apps/be/src/auth/auth.controller.ts
@@ -1,8 +1,10 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { LogInDto } from './dto/login.dto';
 import { SignUpDto } from './dto/sign-up.dto';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
--- apps/be/src/main.ts
+++ apps/be/src/main.ts
@@ -9,6 +9,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 import { PrismaService } from './prisma/prisma.service';
@@ -24,6 +25,31 @@ async function bootstrap() {
 
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
+
+  // app.register(helmet, {
+  //   contentSecurityPolicy: {
+  //     directives: {
+  //       defaultSrc: [`'self'`],
+  //       styleSrc: [`'self'`, `'unsafe-inline'`],
+  //       imgSrc: [`'self'`, 'data:', 'validator.swagger.io'],
+  //       scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+  //     },
+  //   },
+  // });
+
+  // // If you are not going to use CSP at all, you can use this:
+  // app.register(helmet, {
+  //   contentSecurityPolicy: false,
+  // });
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   const port = process.env.PORT || 3001;
   await app.listen(port);
